ecc217e67792c448f86a7e0a9a4502eee1f2ef76
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-09-04 16:29:01 -0300

    fix: attempt to adjust namespaces

--- src/CleanActivitylogCommand.php
+++ src/CleanActivitylogCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Spatie\Activitylog;
+namespace CeruttiMaicon\Activitylog;
 
 use Carbon\Carbon;
 use Illuminate\Console\Command;
--- src/MultiTenantsSeedCommand.php
+++ src/MultiTenantsSeedCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Console\Commands;
+namespace CeruttiMaicon\Commands;
 
 use Illuminate\Console\Command;
 
