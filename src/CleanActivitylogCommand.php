f955ffca4f5d9af8ee6a2e4d3c3586721d2ebaa7
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2022-09-05 14:24:16 -0300

    fix: tweaked clean activity log command to work with tenancy package

--- src/CleanActivitylogCommand.php
+++ src/CleanActivitylogCommand.php
@@ -6,15 +6,19 @@ use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Console\ConfirmableTrait;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class CleanActivitylogCommand extends Command
 {
     use ConfirmableTrait;
 
     protected $signature = 'activitylog:clean
-                            {log? : (optional) The log name that will be cleaned.}
-                            {--days= : (optional) Records older than this number of days will be cleaned.}
-                            {--force : (optional) Force the operation to run when in production.}';
+                            {--table= : (optional) The name of the table to clean}
+                            {--tenants= : (optional) The tenants to be cleaned}
+                            {log? : (optional) The log name that will be cleaned}
+                            {--days= : (optional) Records older than this number of days will be cleaned}
+                            {--force : (optional) Force the operation to run when in production}';
 
     protected $description = 'Clean up old records from the activity log';
 
@@ -24,25 +28,73 @@ class CleanActivitylogCommand extends Command
             return 1;
         }
 
-        $this->comment('Cleaning activity log...');
+        $tenantsAll = \App\Models\Tenant::whereNot('id', 'like', '%_logs')->get();
+        $tenantOption = \App\Models\Tenant::where('id', $this->option('tenants'))->get();
 
-        $log = $this->argument('log');
+        $tenants = $this->option('tenants') === null
+            ? $tenantsAll
+            : $tenantOption;
 
-        $maxAgeInDays = $this->option('days') ?? config('activitylog.delete_records_older_than_days');
+        if ($tenants->count() == 0) {
+            $this->error('Invalid tenant name. Tenant name does not exist.');
+        }
 
-        $cutOffDate = Carbon::now()->subDays($maxAgeInDays)->format('Y-m-d H:i:s');
+        $verifyTable = true;
 
-        $activity = ActivitylogServiceProvider::getActivityModelInstance();
+        foreach ($tenants as $tenant) {
+            tenancy()->initialize($tenant);
 
-        $amountDeleted = $activity::query()
-            ->where('created_at', '<', $cutOffDate)
-            ->when($log !== null, function (Builder $query) use ($log) {
-                $query->inLog($log);
-            })
-            ->delete();
+            $tables = [$this->option('table')];
 
-        $this->info("Deleted {$amountDeleted} record(s) from the activity log.");
+            if (! $this->option('table')) {
+                $tables = DB::select('SHOW TABLES');
 
-        $this->comment('All done!');
+                // NOTE - Return $tables in array
+                $tables = array_map(function ($table) {
+                    return array_values((array) $table)[0];
+                }, $tables);
+
+                $tables = array_diff($tables, ['migrations']);
+
+                $tables[] = 'default';
+
+                $verifyTable = false;
+            }
+
+            if ($verifyTable) {
+                if (! Schema::hasTable($this->option('table'))) {
+                    $this->error('The table '.$this->option('table').' does not exist.');
+                    return 1;
+                }
+            }
+
+            $this->comment("Cleaning activity log for tenant_id: '{$tenant->id}'");
+
+            $this->line('');
+
+            $log = $this->argument('log');
+
+            $maxAgeInDays = $this->option('days') ?? config('activitylog.delete_records_older_than_days');
+
+            $cutOffDate = Carbon::now()->subDays($maxAgeInDays)->format('Y-m-d H:i:s');
+
+            $activity = ActivitylogServiceProvider::getActivityModelInstance();
+
+            $activity->setTenantLog();
+
+            foreach ($tables as $table) {
+                $activity->setTable($table);
+
+                $amountDeleted = $activity->where('created_at', '<', $cutOffDate)
+                    ->when($log !== null, function (Builder $query) use ($log) {
+                        $query->inLog($log);
+                    })
+                    ->delete();
+
+                $this->info("Deleted {$amountDeleted} record(s) from the table: {$table}");
+
+                $this->comment('All done!');
+            }
+        }
     }
 }
