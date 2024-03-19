f216082921e52b6400a1462cec9865ae86b05e7b
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2024-03-18 23:24:23 -0300

    fix: ajustado parâmetro

--- src/MultiTenantsSeedCommand.php
+++ src/MultiTenantsSeedCommand.php
@@ -51,7 +51,7 @@ class MultiTenantsSeedCommand extends Command
                 '--tenants' => $tenant->id,
                 '--class' => $class,
                 // adicionar aqui opção do --force
-                '--force',
+                '--force' => true,
             ]);
         }
     }
